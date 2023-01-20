53130e34f8b6341a92ae717ba1fe078ee9f27ec0
Rafa Mojica <rafael.mojica27@gmail.com>
2023-01-20 00:38:27 -0500

    feature registro, login, persistencia, logout

--- router/users.js
+++ router/users.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const routerUsers = express.Router();
-const axios = require("axios");
+const { register, login, persistence, logout } = require("../controllers/usersControllers");
 
 //CREACION DE USUARIO
-routerUsers.post("/register");
+routerUsers.post("/register", register);
 
 //INICIO DE SESION
-routerUsers.post("/login")
+routerUsers.post("/login", login)
 
 //PERSISTENCIA DE SESION
-routerUsers.get("/me")
+routerUsers.get("/me", persistence)
 
 //CERRAR SESION
-routerUsers.get("/LOGOUT")
+routerUsers.get("/LOGOUT", logout)
 
 module.exports = routerUsers;
